490785a8e76a5fce8c57899cdd8e5077d9b85d8f
Ajaykumar-at-619991248891 <ajaykumar@skilltechnologies.in>
2021-07-07 10:06:04 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
